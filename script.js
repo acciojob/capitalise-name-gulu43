c13546f03b11ed2a717231f74d6466c05c442c2e
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-10 17:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+// function upperCase(str) {
+// 	let txt = fname.getElementById();
+// 	return txt.toUpperCase();
+// }
+document.getElementById("fname").addEventListener("blur", function() {
+    this.value = this.value.toUpperCase();
+});
